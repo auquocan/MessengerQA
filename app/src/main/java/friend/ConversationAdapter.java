c017115a7d9a3d32b6f9bc26b70a9badeb5372bd
dangquocan <arcquocan@gmail.com>
2016-06-09 15:25:36 +0700

    Official

---
+++ app/src/main/java/friend/ConversationAdapter.java
@@ -0,0 +1,79 @@
+package friend;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import chat.ChatMessage;
+import quytrinh.quocan.quocan.messengerqa.MainActivity;
+import quytrinh.quocan.quocan.messengerqa.R;
+
+/**
+ * Created by MyPC on 06/06/2016.
+ */
+public class ConversationAdapter extends ArrayAdapter<ChatMessage> {
+    //Contructor
+    public ConversationAdapter(Context context, int resource, List<ChatMessage> items) {  //Constructor
+        super(context, resource, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.row_friend_conversation, null);
+        }
+
+        ChatMessage p = getItem(position);
+
+        if (p != null) {
+            TextView txt_name = (TextView) v.findViewById(R.id.textViewTenChat);
+            if(MainActivity.user_key.equals(p.userEmail))// if this one send chat set his image
+                txt_name.setText(p.fullName_2);
+            else
+                txt_name.setText(p.fullName);
+
+
+            TextView txt_cur_chat = (TextView) v.findViewById(R.id.textViewCurrentChat);
+            txt_cur_chat.setText(p.message);
+
+            ImageView img_conver  = (ImageView) v.findViewById(R.id.imageConversation);
+            Bitmap bitmap;
+            if(MainActivity.user_key.equals(p.userEmail))// if this one send chat set his image
+                 bitmap  = StringToBitMap(p.imgUserChat_2);
+            else
+                bitmap  = StringToBitMap(p.imgUserChat);
+            img_conver.setImageBitmap(bitmap);
+
+        }
+
+        return v;
+    }
+    //String to BitMap
+    public Bitmap StringToBitMap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+}
+
+
